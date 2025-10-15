b10f083285f2b5b42796e853e1f51b4389e67194
Vesper <xgq2451115363@163.com>
2025-10-15 18:27:59 +0800

    增加部分逻辑function

--- main/Int/Int_BDR6120S.c
+++ main/Int/Int_BDR6120S.c
@@ -4,7 +4,20 @@ void Int_BDR6120S_Init(void)
 {
     MY_LOGI("Int_BDR6120S_Init");
 }
+void Int_BDR6120S_Forward(void)
+{
+    MY_LOGI("Int_BDR6120S_Forward");
+}
 
+void Int_BDR6120S_Backward(void)
+{
+    MY_LOGI("Int_BDR6120S_Backward");
+}
+
+void Int_BDR6120S_Stop(void)
+{
+    MY_LOGI("Int_BDR6120S_Stop");
+}
 void Int_BDR6120S_Unlock(void)
 {
     MY_LOGI("Int_BDR6120S_Unlock");
