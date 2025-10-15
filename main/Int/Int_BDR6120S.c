e0ccfd71e6740c8246fe0f1d68bd30016f64c26f
Vesper <xgq2451115363@163.com>
2025-10-15 18:24:23 +0800

    开始写电机Int_BDR6120S驱动

---
+++ main/Int/Int_BDR6120S.c
@@ -0,0 +1,11 @@
+#include "Int_BDR6120S.h"
+
+void Int_BDR6120S_Init(void)
+{
+    MY_LOGI("Int_BDR6120S_Init");
+}
+
+void Int_BDR6120S_Unlock(void)
+{
+    MY_LOGI("Int_BDR6120S_Unlock");
+}
---
+++ main/Int/Int_BDR6120S.h
@@ -0,0 +1,17 @@
+#ifndef _INT_BDR6120S_H_
+#define _INT_BDR6120S_H_
+
+
+#include "debug/com_debug.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Int_BDR6120S_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _INT_BDR6120S_H_
